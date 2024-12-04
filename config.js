0850209918118931a01f26b60199c9a4ecb76836
SANTA-MAX <dilshansithum223@gmail.com>
2024-12-04 05:40:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', // Enter Your Session ID
+    SESSION_ID: process.env.SESSION_ID || '2vhllDYZ#5H9lvXuKuPZGhK9p7p8YaElh8fJQjUfa1FE0Hh-oPOs', // Enter Your Session ID
     MONGODB: process.env.MONGODB || '',// Enter Your MongoDB URL
     OWNER_NUMBER: process.env.OWNER_NUMBER || '',    // Enter Your Owner Number
 };
